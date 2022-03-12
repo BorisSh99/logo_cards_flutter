9a358b5886f75ae822b25b1e5880b43974d46f26
BorisShek <Boris.Shek@haw-hamburg.de>
2022-03-12 21:59:08 +0100

    - Added animations to FLoatingActionButton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '../widgets/add_card_pack_widget.dart';
 import '../widgets/rename_card_pack_widget.dart';
 import '../models/card_model.dart';
@@ -37,6 +38,9 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  final duration = Duration(milliseconds: 300);
+  bool _isFabVisible = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,123 +48,130 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('My Card-Collections'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        label: Text('ADD NEW PACK'),
-        icon: Icon(Icons.add),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AddCardPackWidget(
-                cardPackList: _cardPackList,
-                addCardPackHandler: _addCardPack,
-              );
-            },
-          );
-        },
+      floatingActionButton: AnimatedSlide(
+        duration: duration,
+        offset: _isFabVisible ? Offset.zero : Offset(0, 2),
+        child: FloatingActionButton.extended(
+          label: Text('ADD NEW PACK'),
+          icon: Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AddCardPackWidget(
+                  cardPackList: _cardPackList,
+                  addCardPackHandler: _addCardPack,
+                );
+              },
+            );
+          },
+        ),
       ),
-      body: ListView.builder(
-        itemCount: _cardPackList.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
-            child: Material(
-              color: Theme.of(context).cardColor,
-              borderRadius: BorderRadius.circular(12),
-              elevation: 6.0,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (context) => CardPackView(listName: _cardPackList[index].name, cardList: _cardPackList[index].cardList)
-                    ),
-                  );
-                },
-                child: Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(16, 12, 16, 6),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Expanded(
-                        flex: 6,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              _cardPackList[index].name,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
+      body: NotificationListener<UserScrollNotification>(
+        onNotification: (notification) {
+          final ScrollDirection direction = notification.direction;
+          if (direction == ScrollDirection.forward) {
+            if (!_isFabVisible) setState(() => _isFabVisible = true); //performance
+          } else if (direction == ScrollDirection.reverse) {
+            if (_isFabVisible) setState(() => _isFabVisible = false);
+          }
+          return true;
+        },
+        child: ListView.builder(
+          itemCount: _cardPackList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+              child: Material(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(12),
+                elevation: 6.0,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) => CardPackView(listName: _cardPackList[index].name, cardList: _cardPackList[index].cardList)
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(16, 12, 16, 6),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          _cardPackList[index].name,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                          EdgeInsetsDirectional.fromSTEB(0, 6, 0, 0),
+                          child: Text(
+                            'MOCK',
+                            style: TextStyle(
+                              fontSize: 16,
                             ),
-                            Padding(
-                              padding:
-                              EdgeInsetsDirectional.fromSTEB(0, 6, 0, 0),
-                              child: Text(
-                                'MOCK',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                          EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              OutlinedButton(
+                                  onPressed: () {},
+                                  child: Text('BUTTON'),
                               ),
-                            ),
-                            Padding(
-                              padding:
-                              EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.max,
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  OutlinedButton(
-                                      onPressed: () {},
-                                      child: Text('BUTTON'),
-                                  ),
-                                  PopupMenuButton(
-                                    onSelected: (value) async {
-                                      switch (value) {
-                                        case 'rename':
-                                          return showDialog(
-                                            context: context,
-                                            builder: (context) {
-                                              return RenameCardPackWidget(
-                                                cardPack: _cardPackList[index],
-                                                changeNameHandler: (newName) => setState(() { //setState of current cardListName
-                                                  _cardPackList[index].name = newName;
-                                                })
-                                              );
-                                            },
+                              PopupMenuButton(
+                                onSelected: (value) async {
+                                  switch (value) {
+                                    case 'rename':
+                                      return showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          return RenameCardPackWidget(
+                                            cardPack: _cardPackList[index],
+                                            changeNameHandler: (newName) => setState(() { //setState of current cardListName
+                                              _cardPackList[index].name = newName;
+                                            })
                                           );
-                                        case 'delete':
-                                          setState(() {
-                                            _cardPackList.removeAt(index);
-                                          });
-                                      }
-                                    },
-                                    itemBuilder: (context) => [
-                                      const PopupMenuItem(  //onTap pops after executing!
-                                        child: Text('Rename'),
-                                        value: 'rename',
-                                      ),
-                                      const PopupMenuItem(
-                                        child: Text('Delete'),
-                                        value: 'delete',
-                                      ),
-                                    ],
+                                        },
+                                      );
+                                    case 'delete':
+                                      setState(() {
+                                        _cardPackList.removeAt(index);
+                                      });
+                                  }
+                                },
+                                itemBuilder: (context) => [
+                                  const PopupMenuItem(  //onTap pops after executing!
+                                    child: Text('Rename'),
+                                    value: 'rename',
+                                  ),
+                                  const PopupMenuItem(
+                                    child: Text('Delete'),
+                                    value: 'delete',
                                   ),
                                 ],
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
--- lib/widgets/card_pack_view.dart
+++ lib/widgets/card_pack_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/rendering.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter/material.dart';
 import '../models/card_model.dart';
@@ -21,109 +22,119 @@ class _CardPackViewState extends State<CardPackView> {
     });
   }
 
+  final duration = Duration(milliseconds: 300);
+  bool _isFabVisible = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.listName),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        label: Text('ADD CARD'),
-        icon: Icon(Icons.add),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AddCardWidget(
-                cardList: widget.cardList,
-                addCardHandler: _addCard,
-              );
-            },
-          );
-        },
+      floatingActionButton: AnimatedSlide(
+        duration: duration,
+        offset: _isFabVisible ? Offset.zero : Offset(0, 2),
+        child: FloatingActionButton.extended(
+          label: Text('ADD CARD'),
+          icon: Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AddCardWidget(
+                  cardList: widget.cardList,
+                  addCardHandler: _addCard,
+                );
+              },
+            );
+          },
+        ),
       ),
-      body: MasonryGridView.count(
-        crossAxisCount: 2,
-        itemCount: widget.cardList.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
-            child: Material(
-              color: Theme.of(context).cardColor,
-              borderRadius: BorderRadius.circular(12),
-              elevation: 6.0,
-              child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(16, 12, 16, 6),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                      flex: 6,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.cardList[index].term,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
+      body: NotificationListener<UserScrollNotification>(
+        onNotification: (notification) {
+          final ScrollDirection direction = notification.direction;
+          if (direction == ScrollDirection.forward) {
+            if (!_isFabVisible) setState(() => _isFabVisible = true); //performance
+          } else if (direction == ScrollDirection.reverse) {
+            if (_isFabVisible) setState(() => _isFabVisible = false);
+          }
+          return true;
+        },
+        child: MasonryGridView.count(
+          crossAxisCount: 2,
+          itemCount: widget.cardList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+              child: Material(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(12),
+                elevation: 6.0,
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 12, 16, 6),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.cardList[index].term,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                        EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                        child: Text(
+                          widget.cardList[index].definition,
+                          style: TextStyle(
+                            fontSize: 16,
                           ),
-                          Padding(
-                            padding:
-                            EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                            child: Text(
-                              widget.cardList[index].definition,
-                              style: TextStyle(
-                                fontSize: 16,
-                              ),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                        EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              splashColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                              hoverColor: Colors.transparent,
+                              icon: Icon(Icons.favorite_border),
+                              onPressed: () {},
                             ),
-                          ),
-                          Padding(
-                            padding:
-                            EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                IconButton(
-                                  splashColor: Colors.transparent,
-                                  highlightColor: Colors.transparent,
-                                  hoverColor: Colors.transparent,
-                                  icon: Icon(Icons.favorite_border),
-                                  onPressed: () {},
+                            PopupMenuButton(
+                              itemBuilder: (context) => [
+                                PopupMenuItem(
+                                  child: Text('Update'),
+                                  value: 1,
                                 ),
-                                PopupMenuButton(
-                                  itemBuilder: (context) => [
-                                    PopupMenuItem(
-                                      child: Text('Update'),
-                                      value: 1,
-                                    ),
-                                    PopupMenuItem(
-                                      child: Text('Delete'),
-                                      value: 2,
-                                      onTap: () {
-                                        setState(() {
-                                          widget.cardList.removeAt(index);
-                                        });
-                                      },
-                                    )
-                                  ],
-                                ),
+                                PopupMenuItem(
+                                  child: Text('Delete'),
+                                  value: 2,
+                                  onTap: () {
+                                    setState(() {
+                                      widget.cardList.removeAt(index);
+                                    });
+                                  },
+                                )
                               ],
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
